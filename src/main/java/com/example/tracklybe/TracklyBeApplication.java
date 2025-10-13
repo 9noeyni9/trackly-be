7a40fabf0f67f155ebbc1ab6397b845a3ea04029
kooinyeong <iykoo@dsmentoring.com>
2025-10-13 10:28:15 +0900

    first commit

---
+++ src/main/java/com/example/tracklybe/TracklyBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tracklybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracklyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracklyBeApplication.class, args);
+    }
+
+}
