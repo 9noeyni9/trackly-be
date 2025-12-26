a8372147c31d9546a5b2e050555558629f9eccb4
9noeyni9 <koo949494@naver.com>
2025-12-27 01:49:41 +0900

    :recycle: refactor: HabitLog 복합키 habit_id, date 생성 및 HabitServiceImpl 사용하지 않는 주입 Repository 삭제

--- src/main/java/com/example/tracklybe/domain/habit/entity/HabitLog.java
+++ src/main/java/com/example/tracklybe/domain/habit/entity/HabitLog.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,7 +22,12 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Entity
-@Table(name = "habit_logs")
+@Table(
+        name = "habit_logs",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"habit_id", "date"})
+        }
+)
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
