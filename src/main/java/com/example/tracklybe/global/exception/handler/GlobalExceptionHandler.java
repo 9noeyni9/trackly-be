3187b9110cb162170975e5dac0440b427252cec6
9noeyni9 <koo949494@naver.com>
2026-01-08 23:34:14 +0900

    :sparkles: feat: HabitLogNotFoundException ExceptionHandler 추가

--- src/main/java/com/example/tracklybe/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/tracklybe/global/exception/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.tracklybe.global.exception.handler;
 
 import com.example.tracklybe.domain.common.dto.ApiError;
 import com.example.tracklybe.domain.common.dto.ApiResponse;
+import com.example.tracklybe.global.exception.HabitLogNotFoundException;
 import com.example.tracklybe.global.exception.HabitNotFoundException;
 import com.example.tracklybe.global.exception.enumeration.ErrorCode;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +17,13 @@ public class GlobalExceptionHandler {
         return buildErrorResponse(e.getErrorCode(), e.getMessage());
     }
 
+    @ExceptionHandler(HabitLogNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleHabitLogNotFoundException(HabitLogNotFoundException e) {
+        return buildErrorResponse(e.getErrorCode(), e.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleGeneralException(Exception ex) {
-        // 내부 메시지 노출 싫으면 detail을 고정 문구로 두는 것도 OK
         return buildErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR, "예기치 않은 오류가 발생했습니다.");
     }
 
